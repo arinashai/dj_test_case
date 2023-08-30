cfd7e3bb997092f087c937fe7c8b6eaf271aacb4
arinashai <shaidurova.arina@gmail.com>
2023-08-30 21:02:16 +0800

    Create models

--- library/lib/models.py
+++ library/lib/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from django.core.exceptions import ValidationError
 
+N_SHELF = 5
+N_SHELF_SLOT = 6
+
 # За каждым залом закреплен библиотекарь.
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
@@ -25,14 +28,47 @@ class Book(models.Model):
 # Поскольку книг много - их хранят в различных залах библиотеки.
 class Hall(models.Model):
 
-    librarian = models.ForeignKey(Librarian, on_delete=models.SET_NULL, null=True)
-    # стеллажи в рамках зала
-    shelves = models.ManyToManyField('Shelf', related_name='hall_shelves')
+    librarian = models.OneToOneField(Librarian, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Hall {self.id}, librarian is {self.librarian}"
 
 # Каждый стеллаж имеет уникальный номер в рамках зала. В каждом зале установлено 5 стеллажей.
 class Shelf(models.Model):
-    number = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+    number = models.PositiveIntegerField(validators=[MaxValueValidator(N_SHELF)])
+
+    def __str__(self):
+        return f"Shelf {self.number} in Hall {self.hall.id}"
+
+class ShelfSlot(models.Model):
+
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+    # На каждом стеллаже есть 6 полок. 
+    slot_number = models.IntegerField(validators=[MaxValueValidator(N_SHELF_SLOT)])
+    books = models.ManyToManyField(Book, through='BookShelf', blank=True)
+
+class BookShelf(models.Model):
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
+    shelf_slot = models.ForeignKey(ShelfSlot, on_delete=models.CASCADE)
+
+class Reader(models.Model):
+    name = models.CharField(max_length=255)
+    borrowed_books = models.ManyToManyField(Book, through='Borrow')
+
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reader = models.ForeignKey(Reader, on_delete=models.CASCADE)
+    borrowed_date = models.DateField()
+    return_date = models.DateField()
+    returned = models.BooleanField(default=False)
+
+# При этом книги периодически перемещаются между залами, стеллажами и полками.
+# За каждым залом закреплен библиотекарь. Все перемещения книг библиотекари отмечают в специальном журнале.
+class BookMovement(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    from_shelf_slot = models.ForeignKey(ShelfSlot, on_delete=models.CASCADE, related_name='from_shelf_slot')
+    to_shelf_slot = models.ForeignKey(ShelfSlot, on_delete=models.CASCADE, related_name='to_shelf_slot')
+    date = models.DateField()
