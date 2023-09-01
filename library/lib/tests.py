582740dc3285c9592d708efa7bb1529f68063ef2
arinashai <shaidurova.arina@gmail.com>
2023-09-01 14:45:26 +0800

    Add test for returning_book

--- library/lib/tests.py
+++ library/lib/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from lib.models import Book, Reader, Borrow
+from lib.models import Book, Reader, Librarian, Hall, Shelf, ShelfSlot, Borrow
 from lib.methods import borrowing_book, MAX_READER_BOOK
-from django.utils import timezone
-from datetime import timedelta
+from lib.methods import returning_book
+from lib.models import N_SHELF, N_SHELF_SLOT
 
 class BorrowingBookTestCase(TestCase):
+
     def setUp(self):
         self.reader = Reader.objects.create(name='Test reader')
         self.book = Book.objects.create(title='Test book', 
@@ -35,3 +36,40 @@ class BorrowingBookTestCase(TestCase):
         borrowing_book(self.reader, book3)   
         result = borrowing_book(self.reader, self.book)
         self.assertEqual(result, f'Читатель уже взял максимальное количество книг: {MAX_READER_BOOK}.')
+
+class ReturningBookTestCase(TestCase):
+
+    def setUp(self):
+        self.reader = Reader.objects.create(name='Test reader')
+        self.book = Book.objects.create(title='Test book', 
+                                        page_count=100, 
+                                        publication_date=2023)
+
+        self.librarian = Librarian.objects.create(name='Test librarian')  
+        self.hall = Hall.objects.create(librarian=self.librarian)
+        
+        # Создание стеллажей и полок
+        for num_shelf in range(1, N_SHELF+1):
+            shelf = Shelf.objects.create(number=num_shelf, hall=self.hall)
+            for num_slot in range(1, N_SHELF_SLOT+1):
+                ShelfSlot.objects.create(shelf=shelf, slot_number=num_slot)
+
+        Borrow.objects.create(reader=self.reader, book=self.book, returned=False, borrow_date="2022-01-01", return_date="2022-02-01")
+
+
+    def test_returning_book(self):
+        result = returning_book(self.reader, self.book)
+        self.assertEqual(result, 'Книга успешно возвращена.')
+    
+    def test_returning_unavailable_book(self):
+        book1 = Book.objects.create(title='Другая книга', 
+                                    page_count=100, 
+                                    publication_date=2023)
+        result = returning_book(self.reader, book1)
+        self.assertEqual(result, 'Читатель не может вернуть книгу, которую не брал.')
+    
+    def test_returning_twice_book(self):
+        returning_book(self.reader, self.book)
+        result = returning_book(self.reader, self.book)
+        self.assertEqual(result, 'Читатель уже вернул книгу.')
+    
